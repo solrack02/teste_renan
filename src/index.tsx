c5270ed2e64e3488bc960e40b9fd24e83e0f2e6f
Carlos A. <carlosvsk@gmail.com>
2025-05-20 14:24:58 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -29,15 +29,7 @@
         (...args: any) => <Elements.Screen3 pass={{
           pathScreen:"home",
 
-          styles:[
-              {
-                backgroundColor: '#101',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
-                width: '100%',
-              }
-              ],
+          styles:[`{ alignItems: "center" }`],
 
           screenElements:[
         
